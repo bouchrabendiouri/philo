6d37c7955573e312babeb7e19f863aea711ded6a
bouchrabendiouri <60324500+bouchrabendiouri@users.noreply.github.com>
2023-05-31 16:02:49 +0100

    time.c

--- time.c
+++ time.c
@@ -19,14 +19,3 @@ long int time_now(void)
     gettimeofday(&now, NULL);
     return ((now.tv_sec * 1000) + (now.tv_usec / 1000));
 }
-
-int func_usleep(long int time)
-{
-    long int start_time;
-
-    start_time = time_now();
-    while ((time_now() - start_time) < time)
-        usleep(100);
-    return (1);
-}
-
