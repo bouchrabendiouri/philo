0eeb10cc084031121211dbc88f7cbfb381f6e457
bouchrabendiouri <60324500+bouchrabendiouri@users.noreply.github.com>
2023-06-23 16:20:24 +0100

    time.c

--- time.c
+++ time.c
@@ -12,10 +12,20 @@
 
 #include "philo.h"
 
-long int time_now(void)
+useconds_t time_now(void)
 {
     struct timeval now;
 
     gettimeofday(&now, NULL);
     return ((now.tv_sec * 1000) + (now.tv_usec / 1000));
+}
+
+void    my_usleep(useconds_t time)
+{
+    useconds_t start_time = time_now();
+    usleep((time * 1000) - 20000);
+    while (time_now() - start_time < time)
+    {
+        
+    }
 }
