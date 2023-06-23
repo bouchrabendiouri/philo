456a9d292bc9f5e357a8551f411faccf09a89461
bouchrabendiouri <60324500+bouchrabendiouri@users.noreply.github.com>
2023-06-23 16:19:48 +0100

    philo.h

--- philo.h
+++ philo.h
@@ -26,23 +26,28 @@ typedef struct s_print{
 	pthread_mutex_t		luck;
 }	t_print;
 
-typedef struct s_philo{
+typedef struct s_data{
     int time_to_die;
     int time_to_eat;
     int time_to_sleep;
-    long int last_sleep;
     long int time_of_day;
+    long int time_creat;
+    long int last_meal;
+    long int last_sleep;
     int index;
     int nbr_philo;
     int	ac;
-    long int last_meal;
+     int	nbr_eat;
+}   t_data;
+
+typedef struct s_philo{
+   
     t_print *print;
-    long int time_creat;
-    int	nbr_eat;
+    t_data *data;
     pthread_mutex_t *mutex;
 }   t_philo;
 
-long int time_now(void);
+useconds_t time_now(void);
 int	check_argument(char **av);
 void creat_philo(t_philo *var, pthread_t *th);
 void	print_msg(char *str, t_philo *var, int i);
@@ -50,5 +55,6 @@ int	check_die(t_philo *var);
 int	ft_atoi(char *str);
 void	corr_print(char *str, t_philo *var, int i);
 void creat_philo(t_philo *var, pthread_t *th);
+void    my_usleep(useconds_t time);
 
 #endif
