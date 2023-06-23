bda9644277858c193b314bab49954ec86a53a17d
bouchrabendiouri <60324500+bouchrabendiouri@users.noreply.github.com>
2023-06-23 16:18:59 +0100

    philo.c

--- philo.c
+++ philo.c
@@ -40,9 +40,8 @@ int	check_argument(char **av)
 }
 void	ft_helper(t_philo *var)
 {
-	var->last_sleep = time_now();
-	while (time_now() - var->last_sleep < var->time_to_sleep)
-		usleep(100);
+	print_msg("is sleeping", var, 0);
+	my_usleep(var->data->time_to_sleep);
 	print_msg("is thinking", var, 0);
 }
 void *func_routine(void *philo)
@@ -52,24 +51,19 @@ void *func_routine(void *philo)
     var = (t_philo *)philo;
     while (1)
     {
-        pthread_mutex_lock(&var->mutex[var->index]);
+        pthread_mutex_lock(&var->mutex[var->data->index]);
         print_msg("has taken a fork", var, 0);
-        pthread_mutex_lock(&var->mutex[(var->index + 1) % var->nbr_philo]);
+        pthread_mutex_lock(&var->mutex[(var->data->index + 1) % var->data->nbr_philo]);
         print_msg("has taken a fork", var, 0);
         print_msg("is_eating", var, 0);
-        pthread_mutex_lock(&var[0].print->luck);
-		var->last_meal = time_now();
-		if (var->nbr_eat > 0)
-			var->nbr_eat--;
-		pthread_mutex_unlock(&var[0].print->luck);
-		while (time_now() - var->last_meal < var->time_to_eat)
-			usleep(100);
-		pthread_mutex_unlock(&var->mutex[var->index]);
-		pthread_mutex_unlock(&var->mutex[(var->index + 1) % var->nbr_philo]);
-		if (var->nbr_eat == 0)
-			return (NULL);
-		print_msg("is sleeping", var, 0);
+		var->data->last_meal = time_now();
+		if (var->data->nbr_eat > 0)
+			var->data->nbr_eat--;
+		my_usleep(var->data->time_to_eat);
+		pthread_mutex_unlock(&var->mutex[var->data->index]);
+		pthread_mutex_unlock(&var->mutex[(var->data->index + 1) % var->data->nbr_philo]);
 		ft_helper(var);
+		usleep(100);
     }
     return (NULL);
 }
@@ -78,16 +72,15 @@ void helper_philo(t_philo *var, pthread_t *th)
 	int i;
 
 	i = 1;
-	 while (i < var->nbr_philo)
+	 while (i < var->data->nbr_philo)
     {
-       var[i].time_creat = var[0].time_creat;
-	   var[i].last_meal = var[0].time_creat;
+       var->data[i].time_creat = var->data[0].time_creat;
+	   var->data[i].last_meal = var->data[0].time_creat;
         if (pthread_create(&th[i], NULL, func_routine, &var[i]))
         {
             printf("Error\n");
             return ;
         }
-        usleep(100);
         i += 2;
     }
 }
@@ -96,40 +89,35 @@ void creat_philo(t_philo *var, pthread_t *th)
     int i;
 
     i = 0;
-	var[i].time_creat = time_now();
-    while (i < var->nbr_philo)
+	var->data[i].time_creat = time_now();
+    while (i < var->data->nbr_philo)
     {
-		var[i].time_creat = var[0].time_creat;
-		var[i].last_meal = var[0].time_creat;
+		var->data[i].time_creat = var->data[0].time_creat;
+		var->data[i].last_meal = var->data[0].time_creat;
         if (pthread_create(&th[i], NULL, func_routine, &var[i]))
         {
             printf("Error\n");
             return ;
         }
-        usleep(100);
         i += 2;
     }
+    usleep(100);
 	helper_philo(var, th);
-    i = 0;
-    while (i < var->nbr_philo)
-	{
-		pthread_detach(th[i]);
-		i++;
-	}
 }
 
 void	print_msg(char *str, t_philo *var, int i)
 {
-	pthread_mutex_lock(&var->print->print);
+	if (!i)
+		pthread_mutex_lock(&var->print->print);
 	if (i == 1)
 	{
-		printf("%s%ld ms %d died\n", RED, time_now() - var->time_creat, var->index + 1);
+		printf("%s%ld ms %d died\n", RED, time_now() - var->data->time_creat, var->data->index + 1);
 		return ;
 	}
 	else
 	{
-		printf("%ld ms %d %s\n", time_now() - var->time_creat, \
-			var->index + 1, str);
+		printf("%ld ms %d %s\n", time_now() - var->data->time_creat, \
+			var->data->index + 1, str);
 	}
 	pthread_mutex_unlock(&var->print->print);
 }
@@ -139,24 +127,25 @@ int	check_die(t_philo *var)
 	int		i;
 	int		j;
 
+	(void)var;
 	while (1)
 	{
 		i = -1;
 		j = 0;
-		while (++i < var[0].nbr_philo)
+		while (++i < var->data[0].nbr_philo)
 		{
-			pthread_mutex_lock(&var[0].print->luck);
-			if (var[i].ac == 6 && var[i].nbr_eat == 0)
+			if (var->data[i].ac == 6 && var->data[i].nbr_eat == 0)
 				j++;
-			if (j == var[i].nbr_philo)
+			if (j == var->data[i].nbr_philo)
 				return (0);
-			if (time_now() - var[i].last_meal >= var[i].time_to_die)
+			if (time_now() - var->data[i].last_meal >= var->data[i].time_to_die)
 			{
+				pthread_mutex_lock(&var->print->print);
 				print_msg("", var, 1);
 				return (0);
 			}
-			pthread_mutex_unlock(&var[0].print->luck);
 		}
+		usleep(100);
 	}
 	return (0);
 }
